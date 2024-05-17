456aa1d2987be010f36f9aa89d9f54a3707034e3
dannyp0930 <dannyp0930@gmail.com>
2024-05-17 15:13:14 +0900

    feat: dvh

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,8 @@ import Home from "@/pages/home";
 import Article from "@/pages/article";
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
-import { useEffect } from "react";
 
 export default function App() {
-  function setScreenSize() {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }
-  
-  useEffect(() => {
-    setScreenSize();
-    window.addEventListener("resize", setScreenSize)
-  }, []);
-
   return (
     <main className="app">
       <BrowserRouter>
