58e89c934c258118abaf04366f24a0758a665f1c
dannyp0930 <dannyp0930@gmail.com>
2024-05-26 21:29:29 +0900

    chore: tailwind/gitignore 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
